9fc425331dc87f5842a726831bebec28a498aa44
Igorek95 <129251185+Igorek95@users.noreply.github.com>
2023-09-10 19:11:54 +0300

    Update vacancy.py

--- parsing_of_vacancies/parsing_of_vacancies/vacancy.py
+++ parsing_of_vacancies/parsing_of_vacancies/vacancy.py
@@ -32,9 +32,9 @@ class Vacancy:
             responsibilities (str): Описание обязанностей.
         """
         self.name_job = name_job
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.currency = currency
+        self.salary_from = int(salary_from)
+        self.salary_to = int(salary_to)
+        self.currency = str(currency)
         self.link = link
         self.address = address
         self.responsibilities = responsibilities
